5e552ecffefb721ff5ed3f178e03133cc9fc7ca9
Luis Carli <luiscarli@kensho.com>
2016-01-18 17:48:49 -0500

    bates: add bundle, dust and cov cmds

--- bin/bates.sh
+++ bin/bates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export LOCAL=node_modules/bates
+export BATES_PTH=node_modules/bates
 export PATH=$PATH:$(pwd)/node_modules/.bin
 
-DEV_SERVER=$LOCAL/src/devServer
-TEST=$LOCAL/bin/test.sh
+DEV_SERVER=$BATES_PTH/src/devServer
+TEST=$BATES_PTH/bin/test.sh
 
 if [ $1 = "start" ]; then
   echo "npm prune, npm start"
@@ -29,8 +29,30 @@ if [ $1 = "lib" ]; then
   --ignore *.test.js \
   --out-dir lib
 fi
-if [ $1 = "main" ]; then
+if [ $1 = "bundle" ]; then
   NODE_ENV=production
   webpack \
-  --config $LOCAL/src/webpackProd
+  --config $BATES_PTH/src/webpackBundle
+fi
+if [ $1 = "dist" ]; then
+  NODE_ENV=production
+  webpack \
+  --config $BATES_PTH/src/webpackDist
+fi
+if [ $1 = "cov" ]; then
+  if [ ! -e .babelrc ]; then
+    HAS_BABELRC=false
+    echo '{"presets":["react","es2015","stage-0"]}' > .babelrc
+  fi
+  NODE_ENV=test
+  babel-node \
+  node_modules/.bin/isparta cover \
+  --report text --report lcovonly --report html \
+  --include 'src/**/!(*-test).js' \
+  node_modules/.bin/_mocha \
+  -- -R dot 'src/**/*.test.js'
+  open coverage/index.html
+  if [ $HAS_BABELRC = "false" ]; then
+    rm -rf .babelrc
+  fi
 fi
