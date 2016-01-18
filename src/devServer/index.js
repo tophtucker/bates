dc7617713f0a7e71c1f277905a2d3778b0ba6157
Luis Carli <luiscarli@kensho.com>
2016-01-18 18:51:32 -0500

    devServer: switch to 200.html

--- src/devServer/index.js
+++ src/devServer/index.js
@@ -5,8 +5,8 @@ var express = require('express')
 var webpack = require('webpack')
 var config = require('../webpackDev')
 
-const app = express()
-const compiler = webpack(config)
+var app = express()
+var compiler = webpack(config)
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
@@ -14,7 +14,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler))
 app.use(express.static(path.join(process.cwd(), 'dist')))
 app.get('*', function readFile(req, res) {
-  res.sendFile(path.join(process.cwd(), 'dist/index.html'))
+  res.send(path.join(process.cwd(), 'dist/200.html'))
 })
 app.listen(3000, 'localhost', function startServer(err) {
   if (err) {
