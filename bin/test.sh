8a794ec551cd581a3b4ad56710796dd797a5e409
Luis Carli <luiscarli@kensho.com>
2016-01-19 14:55:06 -0500

    (FIX) testhook: new babelhook for test

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 NODE_ENV=test
-mocha --require $BATES_PTH/src/babelhook/index.js -R dot "src/**/*.test.js" && eslint src -c $BATES_PTH/.eslintrc
+mocha \
+--require $BATES_PTH/src/testhook/index.js \
+-R dot "src/**/*.test.js"
+eslint src -c $BATES_PTH/.eslintrc
