deaff74b61202a66d9f947748afc6aea57242ca8
Luis Carli <luiscarli@kensho.com>
2016-05-09 15:39:26 -0400

    (FIX) comment test template

--- template/main.test.js
+++ template/main.test.js
@@ -1,12 +1,12 @@
-import {it as test} from 'mocha'
-import assert from 'assert'
-
-import React from 'react'
-import shallowRender from '@luiscarli/shallow-render'
-
-import {Root} from './main'
-
-test('Root', () => {
-  const component = shallowRender(<Root/>)
-  assert.deepEqual(component.type, 'div')
-})
+// import {it as test} from 'mocha'
+// import assert from 'assert'
+//
+// import React from 'react'
+// import shallowRender from '@luiscarli/shallow-render'
+//
+// import {Root} from './main'
+//
+// test('Root', () => {
+//   const component = shallowRender(<Root/>)
+//   assert.deepEqual(component.type, 'div')
+// })
