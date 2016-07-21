e4918ed9b345078a1fea3cc670f0d681893207a1
Luis Carli <luiscarli@kensho.com>
2016-07-21 10:27:52 -0400

    (FTR) package: add npm-install-webpack-plugin

--- src/webpackDev/index.js
+++ src/webpackDev/index.js
@@ -1,6 +1,7 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
 
 module.exports = {
   devtool: 'eval',
@@ -14,18 +15,28 @@ module.exports = {
     publicPath: '/'
   },
   plugins: [
+    new NpmInstallPlugin({
+      dev: true,
+      peerDependencies: false,
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(process.cwd(), 'src'),
-      query: {
-        extends: path.join(__dirname, '../../.babelrc'),
-        // cacheDirectory: true,
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: path.join(process.cwd(), 'src'),
+        query: {
+          extends: path.join(__dirname, '../../.babelrc'),
+          // cacheDirectory: true,
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
       }
-    }]
+    ]
   }
 }
