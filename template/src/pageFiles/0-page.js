2d3707b37ca77add5fa60bbfdfd6bc0815b4157a
Luis Carli <luiscarli@kensho.com>
2016-09-01 14:38:11 -0400

    template: update pageFiles

--- template/src/pageFiles/0-page.js
+++ template/src/pageFiles/0-page.js
@@ -1,6 +1,5 @@
 
 export const title = 'Page 0'
-export const description = ''
 export const hide = false
 
 import React from 'react'
@@ -8,8 +7,12 @@ import {ui} from 'stijl'
 
 export default class Page0 extends React.Component {
   render() {
-    return <ui.Col>
-      <ui.H1>{title}</ui.H1>
-    </ui.Col>
+    return <ui.Row padding>
+      <ui.Col marginAuto flexShrink css={{
+        flexBasis: 600,
+      }}>
+        <ui.H1>{title}</ui.H1>
+      </ui.Col>
+    </ui.Row>
   }
 }
